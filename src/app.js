d622eeb57f3ab06a7d3a3db63021b3ed1833f1a7
Eduardo <eduardokunmixyeah@gmail.com>
2023-11-11 13:16:08 -0400

    config package

--- src/app.js
+++ src/app.js
@@ -2,3 +2,11 @@ import express from "express";
 import morgan from "morgan";
 
 const app = express();
+
+// Settings
+app.set("port",3000);
+
+// Middlewares
+app.use(morgan("dev"))
+
+export default app;
