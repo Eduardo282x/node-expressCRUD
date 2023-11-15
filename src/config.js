fe45d05f4f850143c36fe2964a0e507d87fdd0a5
Eduardo <eduardokunmixyeah@gmail.com>
2023-11-14 20:27:02 -0400

    fix code

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 export default {
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    host: process.env.HOST || '',
+    database: process.env.DATABASE || '',
+    user: process.env.USER || '',
+    password: process.env.PASSWORD || '',
 };
