0cbd90505552db1e3fbdda4fd628f9816b5864f4
Eduardo <eduardokunmixyeah@gmail.com>
2023-11-16 23:27:19 -0400

    add endpoint to get inventory

--- src/controllers/inventory.controller.js
+++ src/controllers/inventory.controller.js
@@ -2,10 +2,17 @@ import { getConnection }  from '../database/database'
 
 const getInventory = async (req, res) =>{
     const connection = await getConnection();
-    const result = await connection.query('SELECT * FROM inventory');
+    const result = await connection.query('SELECT inventory.Id, inventory.Description, inventory.Amount, aulas.Aula AS Classroom, users.Name as Professor, inventory.Date  FROM inventory left join aulas on inventory.Classroom = aulas.Id left join users on inventory.Professor = users.Id');
+    res.json(result);
+}
+
+const addInventory = async (req, res) =>{
+    const connection = await getConnection();
+    const result = await connection.query('INSERT INTO inventory');
     res.json(result);
 }
 
 export const methods = {
-    getInventory
+    getInventory,
+    addInventory
 };
