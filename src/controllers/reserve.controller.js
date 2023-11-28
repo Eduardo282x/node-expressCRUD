60d7199264fd5426af49720b6fb0f7e162625934
Eduardo <eduardokunmixyeah@gmail.com>
2023-11-28 00:07:31 -0400

    send id in reserve

--- src/controllers/reserve.controller.js
+++ src/controllers/reserve.controller.js
@@ -4,10 +4,10 @@ const getReserve = async (req, res) =>{
     const { IdProfesor } = req.body;
     const connection = await getConnection();
     if(IdProfesor != 1){
-        const result = await connection.query(`SELECT users.Name, inventory.Description, reserve.Uses, reserve.HourStart, reserve.HourEnd FROM reserve left join inventory on reserve.IdInventario = inventory.Id left join users on reserve.IdProfesor = users.id where reserve.IdProfesor = ${IdProfesor}`);
+        const result = await connection.query(`SELECT reserve.Id, users.Name, inventory.Description, reserve.Uses, reserve.HourStart, reserve.HourEnd FROM reserve left join inventory on reserve.IdInventario = inventory.Id left join users on reserve.IdProfesor = users.id where reserve.IdProfesor = ${IdProfesor}`);
         res.json(result);
     } else {
-        const resultAdmin = await connection.query('SELECT users.Name, inventory.Description, reserve.Uses, reserve.HourStart, reserve.HourEnd FROM reserve left join inventory on reserve.IdInventario = inventory.Id left join users on reserve.IdProfesor = users.id');
+        const resultAdmin = await connection.query('SELECT reserve.Id, users.Name, inventory.Description, reserve.Uses, reserve.HourStart, reserve.HourEnd FROM reserve left join inventory on reserve.IdInventario = inventory.Id left join users on reserve.IdProfesor = users.id');
         res.json(resultAdmin);
     }
 }
